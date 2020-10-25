2beb619cc70035adcaf9995f8360344042a634c9
Thomas Oliver <blueskin@gmail.com>
2020-10-25 19:51:38 +0100

    ex01

---
+++ d03/ex01/FragTrap.hpp
@@ -0,0 +1,42 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include <iostream>
+
+class FragTrap {
+	public :
+		FragTrap(void);
+		FragTrap(FragTrap const &src);
+		FragTrap(std::string name);
+		~FragTrap(void);
+
+
+		static unsigned int const	maxhp = 100;
+		static unsigned int const	maxep = 100;
+
+		FragTrap & operator=(FragTrap const &rhs);
+		
+		void				rangedAttack(std::string const &target) const;
+		void				meleeAttack(std::string const &target) const;
+		void				takeDamage(unsigned int amount);
+		void				beRepaired(unsigned int amount);
+		void				vaulthunter_dot_exe(std::string const &target);
+
+		unsigned int		getHp(void) const;
+		unsigned int		getEp(void) const;
+		unsigned int		getLevel(void) const;
+		std::string const	&getName(void) const;
+		unsigned int		getMeleeDamage(void) const;
+		unsigned int		getRangedDamage(void) const;
+		unsigned int		getArmorReduc(void) const;
+	private :
+		unsigned int		_hp;
+		unsigned int		_ep;
+		unsigned int		_level;
+		std::string			_name;
+		unsigned int		_meleeDamage;
+		unsigned int		_rangedDamage;
+		unsigned			_armorReduc;
+};
+
+#endif
---
+++ d03/ex01/main.cpp
@@ -0,0 +1,24 @@
+#include "FragTrap.hpp"
+
+int main(void) {
+	FragTrap	robot("Jack");
+	FragTrap	test = robot;
+
+	robot.meleeAttack("enemy");
+	robot.rangedAttack("enemy");
+	robot.takeDamage(30);
+	robot.takeDamage(30);
+	robot.takeDamage(3);
+	robot.takeDamage(30);
+	robot.takeDamage(30);
+	robot.takeDamage(30);
+	robot.beRepaired(40);
+	robot.beRepaired(100);
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.vaulthunter_dot_exe("enemy");
+	robot.meleeAttack("enemy");
+	return 0;
+}
